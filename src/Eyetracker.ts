a678d616db5b58a4bba05ac73b9e24b2b7c41b57
shunjiwan01 <102843220+shunjiwan01@users.noreply.github.com>
2022-06-01 22:22:50 -0400

    isFaceValid debugging plus some formatting

--- src/Eyetracker.ts
+++ src/Eyetracker.ts
@@ -2,6 +2,7 @@ import {
   SupportedModels,
   MediaPipeFaceMeshTfjsModelConfig,
   createDetector,
+  FaceLandmarksDetector,
 } from "@tensorflow-models/face-landmarks-detection";
 import "@tensorflow/tfjs-core";
 import "@tensorflow/tfjs-backend-webgl";
@@ -11,14 +12,12 @@ export class Eyetracker {
 
   async init() {
     const model = SupportedModels.MediaPipeFaceMesh;
-
     const detectorConfig: MediaPipeFaceMeshTfjsModelConfig = {
       runtime: "tfjs",
       refineLandmarks: true,
     };
 
     const detector = await createDetector(model, detectorConfig);
-
     console.log(detector);
   }
 
@@ -35,14 +34,8 @@ export class Eyetracker {
 
   async getCameraPermission(): Promise<any> {
     let stream = null;
-
-    try {
-      stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
-      return stream;
-    } catch (err) {
-      console.log('No available devices or permission rejected.');
-      return null;
-    }
+    stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    return stream;
   }
 
   async getListOfCameras(): Promise<Array<MediaDeviceInfo>> {
@@ -59,73 +52,81 @@ export class Eyetracker {
       d.kind === 'videoinput'
       return d.kind === 'videoinput'
     })
-  
+
     return videoDevices
   }
 
   async setCamera(device: MediaDeviceInfo): Promise<MediaStream> {
     //console.log(device.deviceId)
     // If getListOfCameras uses strings, then deviceId would just be set equal to the parameters
-    const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: device.deviceId }})
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: device.deviceId } })
     return stream
   }
 
-  createDisplay(height: Number, width: Number, Id: string):Object {
-    let display = document.createElement("canvas")
-    display.setAttribute('id', Id);
-    display.style.height = `${height.toString()}px`
-    display.style.width = `${width.toString()}px`
-    //const ctx = display.getContext('2d')
-    return display
+  createDisplay(video: HTMLVideoElement, Id: string): CanvasRenderingContext2D | null {
+    let canvas = document.createElement("canvas")
+    canvas.setAttribute('id', Id);
+    document.body.appendChild(canvas);
+    canvas.style.height = `${video.height.toString()}px`
+    canvas.style.width = `${video.width.toString()}px`
+    canvas.height = video.height;
+    canvas.width = video.width;
+    //const ctx = canvas.getContext('2d')
+    var ctx = canvas.getContext('2d');
+    if (ctx != null) {
+      ctx.translate(canvas.width, 0);
+      ctx.scale(-1, 1);
+      ctx.fillStyle = "green";
+    }
+    return ctx;
   }
 
-  showDisplay(height: Number, width: Number, Id: string, stream: MediaStream):Object {
+  showDisplay(height: number, width: number, Id: string, stream: MediaStream): HTMLVideoElement {
     let video = document.createElement('video')
     video.setAttribute('id', Id)
     document.body.appendChild(video)
     video.style.transform = 'scaleX(-1)'
     video.style.height = `${height.toString()}px`
     video.style.width = `${width.toString()}px`
+    video.height = height;
+    video.width = width;
     video.autoplay = true;
-
     video.srcObject = stream
-    console.log(video)
     return video
   }
 
-  async setFaceLandmarkDetectionModel() {
-    const model = SupportedModels.MediaPipeFaceMesh;
-    const detectorConfig = {
-      runtime: 'mediapipe',
-      refineLandmarks: true,
-      maxFaces: 1,
-      solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
-                    // or 'base/node_modules/@mediapipe/face_mesh' in npm.
-    };
-    // @ts-ignore
-    return detector = await createDetector(model, detectorConfig);
-  }
-  
+  // async setFaceLandmarkDetectionModel() {
+  //   const model = SupportedModels.MediaPipeFaceMesh;
+  //   const detectorConfig = {
+  //     runtime: 'mediapipe',
+  //     refineLandmarks: true,
+  //     maxFaces: 1,
+  //     solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
+  //                   // or 'base/node_modules/@mediapipe/face_mesh' in npm.
+  //   };
+  //   // @ts-ignore
+  //   return detector = await createDetector(model, detectorConfig);
+  // }
+
   async faceLandmarkDetection() {
     const model = SupportedModels.MediaPipeFaceMesh;
-
     const detectorConfig: MediaPipeFaceMeshTfjsModelConfig = {
       runtime: "tfjs",
       maxFaces: 1,
       refineLandmarks: true,
     };
 
-    console.log(model)
     const detector = await createDetector(model, detectorConfig);
-
     return detector
   }
 
   async isFaceValid(video: HTMLVideoElement, detector: any): Promise<any> {
-    const faces = await detector.estimateFaces(video, {flipHorizontal: true});
-    if(faces.length > 0) {console.log('FACE DETECTED!')}
-    else {console.log('waiting for faces...')}
+    const faces = await detector.estimateFaces(video, { flipHorizontal: true })
+    if (faces.length > 0) {
+      console.log('FACE DETECTED');
+      // console.log(faces)
+    }
+    else { console.log('Waiting for faces...') }
     window.requestAnimationFrame(await this.isFaceValid(video, detector));
   }
-
 }
